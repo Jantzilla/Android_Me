d6d32af26dc475cc7e2e5133bb087948b8895cd7
Jantz <jantzillac.93@gmail.com>
2018-05-14 02:02:43 -0500

    Implement the MasterListFragment callback, OnImageClickListener

--- app/src/main/java/com/example/android/android_me/ui/MainActivity.java
+++ app/src/main/java/com/example/android/android_me/ui/MainActivity.java
@@ -22,14 +22,19 @@ import android.support.v7.app.AppCompatActivity;
 import com.example.android.android_me.R;
 
 // This activity is responsible for displaying the master list of all images
-// TODO (4) Implement the MasterListFragment callback, OnImageClickListener
-public class MainActivity extends AppCompatActivity {
+// COMPLETED (4) Implement the MasterListFragment callback, OnImageClickListener
+public class MainActivity extends AppCompatActivity implements MasterListFragment.OnImageClickListener{
 
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+    }
+
+    @Override
+    public void onImageSelected(int position) {
 
     }
 
